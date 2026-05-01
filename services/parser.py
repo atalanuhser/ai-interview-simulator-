e55953b4ef8d0e65e230d0a25336cc9a47882408
Cemre Yurtsever <90215801+cemreeyrtsvr@users.noreply.github.com>
2026-05-01 15:51:45 +0300

    Delete services directory

--- services/parser.py
+++
@@ -1,9 +0,0 @@
-from PyPDF2 import PdfReader
-
-def pdf_to_text(file):
-    reader = PdfReader(file)
-    text = ""
-    for page in reader.pages:
-        if page.extract_text():
-            text += page.extract_text()
-    return text
